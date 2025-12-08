a54a211dbef8f5fc96c1f0de9f5877766e13318c
Eshwar-fut-dev <eshwar02.2005@gmail.com>
2025-12-08 11:47:33 +0530

    Refactored Guild/GuildsRail

--- components/Navigation/Guild/GuildsRail.tsx
+++ components/Navigation/Guild/GuildsRail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { Plus, Compass } from "lucide-react";
+import { Plus, Compass, MessageSquare } from "lucide-react";
 import { useState } from "react";
 import { cn } from "@/lib/utils/cn";
 import CreateServerModal from "@/components/overlays/CreateServerModal";
@@ -31,7 +31,7 @@ export default function GuildsRail({ activeGuildId }: GuildsRailProps) {
   return (
     <>
       <nav className="w-[72px] h-screen flex flex-col items-center py-3 bg-[#1E1F22] overflow-y-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
-        {/* Home Button (DMs) */}
+        {/* Home Button (DMs) - Replaced Discord logo with chat icon */}
         <div className="mb-2">
           <GuildTooltip label="Direct Messages">
             <button
@@ -43,15 +43,7 @@ export default function GuildsRail({ activeGuildId }: GuildsRailProps) {
                   : "bg-[#313338] hover:bg-[#5865f2] hover:rounded-2xl"
               )}
             >
-              {/* Discord Logo SVG */}
-              <svg
-                width="28"
-                height="20"
-                viewBox="0 0 28 20"
-                className="fill-current text-white"
-              >
-                <path d="M23.0212 1.67671C21.3107 0.879656 19.5079 0.318797 17.6584 0C17.4062 0.461742 17.1749 0.934541 16.9708 1.4184C15.003 1.12145 12.9974 1.12145 11.0283 1.4184C10.819 0.934541 10.589 0.461744 10.3368 0C8.48074 0.324393 6.67795 0.885118 4.96746 1.68231C1.56727 6.77853 0.649666 11.7538 1.11108 16.652C3.10102 18.1418 5.3262 19.2743 7.69177 20C8.22338 19.2743 8.69519 18.4993 9.09812 17.691C8.32996 17.3997 7.58522 17.0424 6.87684 16.6135C7.06531 16.4762 7.24726 16.3387 7.42403 16.1847C11.5911 18.1749 16.408 18.1749 20.5763 16.1847C20.7531 16.3332 20.9351 16.4762 21.1171 16.6135C20.41 17.0369 19.6639 17.3997 18.897 17.691C19.3052 18.4993 19.777 19.2689 20.3031 20C22.6712 19.2689 24.8964 18.1418 26.8876 16.652C27.43 10.9731 25.9665 6.04728 23.0212 1.67671ZM9.68041 13.6383C8.39754 13.6383 7.34085 12.4453 7.34085 10.994C7.34085 9.54272 8.37155 8.34973 9.68041 8.34973C10.9893 8.34973 12.0395 9.54272 12.0187 10.994C12.0187 12.4453 10.9828 13.6383 9.68041 13.6383ZM18.3192 13.6383C17.0363 13.6383 15.9796 12.4453 15.9796 10.994C15.9796 9.54272 17.0103 8.34973 18.3192 8.34973C19.6281 8.34973 20.6783 9.54272 20.6575 10.994C20.6575 12.4453 19.6281 13.6383 18.3192 13.6383Z" />
-              </svg>
+              <MessageSquare size={28} className="text-white" />
 
               {/* Active Indicator */}
               {isDMActive && (
